1460d1eb23ff31bbe7f75bbdabe7f3c5bb4a5e0a
gladys5 <yumanji@live.com>
2022-12-28 14:51:48 -0700

    terminando emiciones prioritarias socket para iniciar singleplayer

---
+++ readme.md
@@ -0,0 +1,3 @@
+# juego tipo Batalla naval
+
+## Socket.IO es una biblioteca que permite la comunicación de baja latencia , bidireccional y basada en eventos entre un cliente y un servidor. Se basa en el protocolo WebSocket y proporciona garantías adicionales, como el respaldo al sondeo largo HTTP o la reconexión automática. Utiliza la coneccion TCP
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.listen(PORT, () => {
   console.log("server is running")
 })
 const connections = [null, null]
-
+//si tenemos un jugador conectado por cada coneccion si hay espacio para otro jugador metelo a la sala lo inicializamos en -1 para que no se conecte a ninguna sala
 io.on("connection", (socket) => {
   let playerIndex = -1
   for (const i in connections) {
@@ -20,22 +20,25 @@ io.on("connection", (socket) => {
       break
     }
   }
+  //le damos el numero de jugador
   socket.emit("player-number", playerIndex)
   console.log(`player ${playerIndex}  is connected!`)
   if (playerIndex === -1) return
   connections[playerIndex] = false
-
+  //le decimos al otro jugador que se conecto un nuevo jugador
   socket.broadcast.emit("player-connection", playerIndex)
-
+  // si el jugador se desconecta
   server.on("disconnect", () => {
     console.log(`player ${playerIndex}  is disconnect!`)
     connections[playerIndex] = null
     socket.broadcast.emit("player-connection", playerIndex)
   })
+  //si el jugador esta listo
   socket.on("player-ready", () => {
     socket.broadcast.emit("enemy-ready", playerIndex)
     connections[playerIndex] = true
   })
+  //si no esta listo y no esta conectado  ponlo en false pero si esta conectado ponlo en true y agregalo a la sala
   socket.on("check-players", () => {
     const players = []
     for (const i in connections) {
@@ -45,4 +48,20 @@ io.on("connection", (socket) => {
     }
     socket.emit("check-players", players)
   })
+  //el jugador disparo lo registramos en la consola y lo enviamos al otro jugador
+  socket.on("fire", (id) => {
+    console.log(`Shot fired from ${playerIndex}`, id)
+    socket.broadcast.emit("fire", id)
+  })
+  //reqistramos a que cuadro le dispararon y lo enviamos al otro jugador
+  socket.on("fire-reply", (square) => {
+    console.log(square)
+    socket.broadcast.emit("fire-reply", square)
+  })
+  //si este tiempo se cumple, y no se ha disparado, se envia un mensaje de timeout y se desconecta del juego
+  setTimeout(() => {
+    connections[playerIndex] = null
+    socket.emit("timeout")
+    socket.disconnect()
+  }, 60000)
 })
