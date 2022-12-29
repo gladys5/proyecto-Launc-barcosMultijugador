ad431d2978c762d0468d32506d37f7e50857dc35
gladys5 <yumanji@live.com>
2022-12-29 05:36:17 -0700

    commit cambios

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require("express")
 const path = require("path")
 const http = require("http")
-const PORT = process.env.PORT || 4001
+const PORT = process.env.PORT || 3000
 const socket = require("socket.io")
 const app = express()
 const server = http.createServer(app)
 const io = socket(server)
-app.use(express.static(path.join(__dirname, "public")))
-server.listen(PORT, () => {
-  console.log("server is running")
-})
+
+app.use(express.static("public"))
+
+server.listen(PORT, () => console.log("El servidor esta corriendo"))
+
 const connections = [null, null]
-//si tenemos un jugador conectado por cada coneccion si hay espacio para otro jugador metelo a la sala lo inicializamos en -1 para que no se conecte a ninguna sala
+
 io.on("connection", (socket) => {
   let playerIndex = -1
   for (const i in connections) {
@@ -20,25 +21,27 @@ io.on("connection", (socket) => {
       break
     }
   }
-  //le damos el numero de jugador
+
   socket.emit("player-number", playerIndex)
-  console.log(`player ${playerIndex}  is connected!`)
+
+  console.log(`Jugador ${playerIndex} se ha conectado`)
+
   if (playerIndex === -1) return
   connections[playerIndex] = false
-  //le decimos al otro jugador que se conecto un nuevo jugador
+
   socket.broadcast.emit("player-connection", playerIndex)
-  // si el jugador se desconecta
-  server.on("disconnect", () => {
-    console.log(`player ${playerIndex}  is disconnect!`)
+
+  socket.on("disconnect", () => {
+    console.log(`Jugador ${playerIndex} se ha desconectado`)
     connections[playerIndex] = null
     socket.broadcast.emit("player-connection", playerIndex)
   })
-  //si el jugador esta listo
+
   socket.on("player-ready", () => {
     socket.broadcast.emit("enemy-ready", playerIndex)
     connections[playerIndex] = true
   })
-  //si no esta listo y no esta conectado  ponlo en false pero si esta conectado ponlo en true y agregalo a la sala
+
   socket.on("check-players", () => {
     const players = []
     for (const i in connections) {
@@ -48,20 +51,20 @@ io.on("connection", (socket) => {
     }
     socket.emit("check-players", players)
   })
-  //el jugador disparo lo registramos en la consola y lo enviamos al otro jugador
+
   socket.on("fire", (id) => {
-    console.log(`Shot fired from ${playerIndex}`, id)
+    console.log(`Disparo de ${playerIndex}`, id)
     socket.broadcast.emit("fire", id)
   })
-  //reqistramos a que cuadro le dispararon y lo enviamos al otro jugador
+
   socket.on("fire-reply", (square) => {
     console.log(square)
     socket.broadcast.emit("fire-reply", square)
   })
-  //si este tiempo se cumple, y no se ha disparado, se envia un mensaje de timeout y se desconecta del juego
+
   setTimeout(() => {
     connections[playerIndex] = null
     socket.emit("timeout")
     socket.disconnect()
-  }, 60000)
+  }, 600000)
 })
